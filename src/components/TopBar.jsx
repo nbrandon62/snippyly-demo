3eac9c865f7f216649055d4dc46eb489a333d89d
nbrandon62 <nbrandon62@gmail.com>
2023-05-22 11:46:47 -0700

    conditionally render the Snippyly components on login. Logout function lives here

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { useSnippylyClient } from '@snippyly/react';
+import { auth } from '../FirebaseConfig';
+import { signOut } from 'firebase/auth';
 import InlineEdit from './InlineEdit';
-import Comment from './Comment'; 
+import Comment from './Comment';
+import Presence from './Presence';
 
-const TopBar = ({ handleLogin, handleLogout, user }) => {
+
+const TopBar = ({ handleLogin, handleSetUser, user }) => {
   const [currentTitle, setCurrentTitle] = useState('Untitled');
   const { client } = useSnippylyClient();
 
@@ -11,12 +15,21 @@ const TopBar = ({ handleLogin, handleLogout, user }) => {
     if (!user) {
       return;
     } else if (client && user) {
-      client.identify(user);
       client.setDocumentId(location.href);
-      return
+      client.identify(user);
     }
-  }, [client]);
+  }, [client, user]);
 
+  const handleLogout = async () => {
+    try {
+      await signOut(auth);
+      client.signOutUser();
+      handleSetUser(null);
+      alert('You have been logged out');
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <div className='bg-[#111111] items-center '>
@@ -37,9 +50,12 @@ const TopBar = ({ handleLogin, handleLogout, user }) => {
             </button>
           )}
         </div>
-        <div className='presence-container'>
-          <Comment />
-        </div>
+        {user ? (
+          <div className='presence-container flex align-center'>
+            <Presence />
+            <Comment />
+          </div>
+        ) : null}
       </div>
     </div>
   );
